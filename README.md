f72e016eb613267e61cac29e35f30e4a5eb079e5
Pontus Lindman <pontus.v.lindman@gmail.com>
2025-02-17 19:06:13 +0200

    Added pytest amd test for health

--- README.md
+++ README.md
@@ -7,9 +7,12 @@ git init
 uv init app 
 uv add fastapi --extra standard
 uv run fastapi dev --help
-uv run fastapi dev --port=8001
+uv run fastapi dev app/main.py --port=8001
 
+uv add pytest
+uv run pytest
 ```
 
 - https://fastapi.tiangolo.com/
 - https://docs.astral.sh/uv/guides/integration/fastapi/
+
---
+++ tests/test_health.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app  # Adjust the import path as necessary
+
+client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "healthy"}
