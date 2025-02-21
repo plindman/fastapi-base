4fdf81ac85eb83b3f144af98f6ac4bff1e08b14d
Pontus Lindman <pontus.v.lindman@gmail.com>
2025-02-21 10:05:03 +0200

    services router default tags and prefix

--- {{cookiecutter.project_slug}}/app/services/__init__.py
+++ {{cookiecutter.project_slug}}/app/services/__init__.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 # from fastapi import Security
 # from ..core.auth_api_key import  verify_api_key
 # router = APIRouter(dependencies=[Security(verify_api_key)])
-router = APIRouter()
+router = APIRouter(tags=["Services"], prefix="/api/v1")
 
-# from .data_service import router as data_router
-# router.include_router(data_router)
+from .data_service import router as data_router
+router.include_router(data_router)
