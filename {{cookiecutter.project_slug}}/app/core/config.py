75dbb64e7d761f79fbdaaf0c88f630a0645848c7
Pontus Lindman <pontus.v.lindman@gmail.com>
2025-02-21 10:20:46 +0200

    Removed unnecessary clutter from file

--- {{cookiecutter.project_slug}}/app/core/config.py
+++ {{cookiecutter.project_slug}}/app/core/config.py
@@ -1,4 +1,3 @@
-import os
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
@@ -6,8 +5,6 @@ from pydantic import Field
 class Settings(BaseSettings):
     app_name: str = "FastAPI Application"
     api_key: str = Field(..., alias="FASTAPI_API_KEY") 
-    # jwt_secret: str = os.getenv("JWT_SECRET", "your_jwt_secret")
-    # database_url: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
     model_config = SettingsConfigDict(env_file=".env", 
                                       env_file_encoding="utf-8",
